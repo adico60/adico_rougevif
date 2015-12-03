a2ab3c134618ec56c56bf66d6633f11c3812f418
YaNn <yann.berton@Yann-PC.domaine.local>
2015-12-03 15:48:11 +0100

    Initial Commit
    Theme's red color

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "pradinestemplate".
+ *
+ * Auto generated 23-08-2014 13:00
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Adico theme RougeVif',
+	'description' => 'Theme RougeVif ',
+	'category' => 'misc',
+	'version' => '0.0.1',
+	'state' => 'stable',
+	'uploadfolder' => true,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Yann Berton',
+	'author_email' => 'yann.berton@adico.fr',
+	'author_company' => 'ADICO',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'bootstrap_package' => '6.2.9-6.99.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
